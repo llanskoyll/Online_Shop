fb2dcdadfd928b68a746bc88b9c976e35e442f14
Danov Konstantin <02danov.konstantin@inbox.ru>
2022-03-09 23:26:29 +0300

    Make CRUD

--- server/ProductController.js
+++ server/ProductController.js
@@ -1,4 +1,5 @@
 import Product from './Product.js'
+import product from "./Product.js";
 
 class PostController{
     async create(req,res) {
@@ -6,6 +7,54 @@ class PostController{
             const {id,name,price} = req.body
             const prod = await Product.create({id,name,price})
             res.status(200).json('Продукт был добавлен!')
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async getAll(req, res) {
+        try {
+            const products = await Product.find()
+            return res.json(products)
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async getOne(req, res) {
+        try {
+            const {id} = req.params
+            if (!id) {
+                return res.status(400).json('No ID in params!')
+            }
+            const product = await Product.findById(id)
+            return res.json(product)
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async Update(req, res) {
+        try {
+            const newProduct = req.body
+            if(!newProduct._id) {
+                return res.status(400).json('No ID in request body!')
+            }
+            const updatedProduct = await Product.findByIdAndUpdate(newProduct._id, newProduct, {new: true})
+            return res.json(updatedProduct)
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            const {id} = req.params
+            if (!id) {
+                return res.status(400).json('No ID in params!')
+            }
+            const product = await Product.findByIdAndDelete(id)
+            return res.json(product)
         } catch (e) {
             res.status(500).json(e)
         }
--- server/route.js
+++ server/route.js
@@ -4,6 +4,11 @@ import ProductController from "./ProductController.js";
 const router = new Router()
 
 router.post('/product', ProductController.create)
+router.get('/product', ProductController.getAll)
+router.get('/product/:id',ProductController.getOne)
+router.put('/product', ProductController.Update)
+router.delete('/product/:id', ProductController.delete)
+
 
 
 export default router;
